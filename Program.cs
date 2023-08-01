a84678c664a2195640f5cb3d8b52420ab0cf9565
singhvishal14 <kumarvishalsingh767@gmail.com>
2023-08-01 09:14:42 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppQuickSort
+{
+    internal class Program
+    {
+        public static void QuickSort(int[] array)
+        {
+            QuickSort(array, 0, array.Length - 1);
+
+        }
+        private static void QuickSort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivotIndex = Partition(array, left, right);
+                QuickSort(array, left, pivotIndex - 1);
+                QuickSort(array, pivotIndex + 1, right);
+
+            }
+        }
+        private static int Partition(int[] array, int left, int right)
+        {
+            int pivot = array[right];
+            int i = left - 1;
+            for (int j = left; j < right; j++)
+            {
+                if (array[j] < pivot)
+                {
+                    i++;
+                    Swap(array, i, j);
+                }
+            }
+            Swap(array, i + 1, right);
+            return i + 1;
+
+
+        }
+        private static void Swap(int[] array, int i, int j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
+        public static void Print(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine("\n");
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { -64, -34, -25, -12, -22, -11, -90 };
+            Print(array);
+            Stopwatch stopwatch =new Stopwatch();
+            stopwatch.Start();
+            QuickSort(array);
+            stopwatch.Stop();
+            Console.WriteLine($"ArraySize {array.Length} Time Taken  {stopwatch.Elapsed.Milliseconds} miliseconds");
+
+            Print(array);
+
+            Console.ReadKey();
+        }
+    }
+}
